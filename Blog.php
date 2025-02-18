2f3781ffbc7ef4f4655fe54bb32f993076f3a6b8
Deekshay22 <deekshayadav4422@gmail.com>
2025-02-18 12:33:01 +0530

    blog page

---
+++ Blog.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="description" content="The team of experienced digital marketers, video creators, website developers and designers offering best solution for your business.">
+    <meta name="keywords" content="The Digital House, Currency Tower, Digital Marketing">
+    <meta name="author" content="The Digital House">
+    <title>Online Advertising | Website Design and Development Company</title>
+    <?php include 'assets/includes/head.php' ?>
+    
+</head>
+
+<body>
+    <?php include 'assets/includes/header.php' ?>
+
+    <div class="page-header bg-section">
+        <div class="page-header-box">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="page-header-content">
+                            <h1 class="wow fadeInUp"><span></span></h1>
+                            <nav class="wow fadeInUp" data-wow-delay="0.25s">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.php">home</a></li>
+                                    <li class="breadcrumb-item" aria-current="page">Our Team</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+   
+
+    
+    
+    <?php include 'assets/includes/footer.php' ?>
+
+<?php include 'assets/includes/scripts.php' ?>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
